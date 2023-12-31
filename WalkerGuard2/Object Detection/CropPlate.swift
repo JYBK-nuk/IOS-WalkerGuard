91aa87448c1772161d345e3d78514b9ce1c63576
henry753851 <henryboy753951@gmail.com>
2024-01-01 06:09:24 +0800

    Fix

--- WalkerGuard2/Object Detection/CropPlate.swift
+++ WalkerGuard2/Object Detection/CropPlate.swift
@@ -33,11 +33,15 @@ class CropPlate{
     static private func cropImage(image: CGImage, rect: CGRect,viewSize:CGSize) -> UIImage? {
         // 裁剪圖像
         // scale rect multiplier image.size/previewSize
-        let scale = CGFloat(image.width)/viewSize.width
+        let scale = CGFloat(image.width)/viewSize.height
+        print("scale = \(scale)")
         let rect = CGRect(x: rect.minX*scale, y: rect.minY*scale, width: rect.width*scale, height: rect.height*scale)
         
+        let transform = CGAffineTransform(rotationAngle: -CGFloat.pi / 2) // 逆時針旋轉90度
+        let rotatedRect = rect.applying(transform).applying(CGAffineTransform(translationX: 0, y: CGFloat(image.height)))
+        
 
-        let croppedCGImage = image.cropping(to: rect)
+        let croppedCGImage = image.cropping(to: rotatedRect)
         if croppedCGImage == nil {
             print("croppedCGImage == nil")
             return nil
--- WalkerGuard2/Service.swift
+++ WalkerGuard2/Service.swift
@@ -2,7 +2,9 @@
 //  Service.swift
 //  WalkerGuard2
 //
-//  Created by 蘇柏諺 on 2024/1/1.
-//
 
 import Foundation
+
+class Service {
+    var detectHistory = [[String]]()
+}
